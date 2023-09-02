1b182d5904296f6b358501ab465b884a7a876220
Xyphyn <james@xylight.dev>
2023-09-02 10:24:54 -0700

    misc: add disabled states

--- src/lib/forms/TextInput.svelte
+++ src/lib/forms/TextInput.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { buttonColor } from '$lib/button/Button.svelte'
+
 	type Size = keyof typeof sizeClass
 
 	const sizeClass = {
@@ -10,7 +12,9 @@
 	export let label: string | undefined = undefined
 	export let value: string = ''
 	export let placeholder: string = ''
+	export let disabled: boolean = false
 	export let size: Size = 'md'
+	export let id: number = Math.floor(Math.random() * 100000000)
 
 	const borderClass = `
 	border border-neutral-200 dark:border-neutral-800
@@ -18,12 +22,14 @@
 </script>
 
 <div class="flex flex-col gap-1">
-	{#if $$slots.label}
-		<span class="text-sm text-neutral-600 dark:text-neutral-400 font-medium">
-			<slot name="label" />
-		</span>
-	{:else if label}
-		<span class="text-sm text-neutral-600 dark:text-neutral-400 font-medium">{label}</span>
+	{#if $$slots.label || label}
+		<label for={id.toString()} class="text-sm text-neutral-600 dark:text-neutral-400 font-medium">
+			{#if $$slots.label}
+				<slot name="label" />
+			{:else if label}
+				{label}
+			{/if}
+		</label>
 	{/if}
 	<div class="rounded-md flex flex-row items-center w-full text-sm {$$props.class}">
 		{#if $$slots.prefix}
@@ -36,15 +42,18 @@
 		{/if}
 		<input
 			type="text"
+			id={id.toString()}
 			{placeholder}
+			{disabled}
 			bind:value
 			on:input
 			on:change
 			{...$$restProps}
-			class="{sizeClass[size]} {borderClass} focus:border-neutral-800
+			class="{sizeClass[size]} {borderClass} hover:bg-white focus:border-neutral-800
 			focus:dark:border-neutral-200 bg-white dark:bg-black
      focus:outline-none focus:ring-2 ring-neutral-800/50 rounded-md
-    dark:ring-neutral-200/50 transition-all text-sm max-w-full {$$props.class}"
+    dark:ring-neutral-200/50 transition-all text-sm max-w-full disabled:bg-neutral-100
+		disabled:cursor-not-allowed disabled:dark:bg-neutral-800 {$$props.class}"
 			class:rounded-l-none={$$slots.prefix}
 			class:rounded-r-none={$$slots.suffix}
 		/>
--- src/routes/page/components/Navbar.svelte
+++ src/routes/page/components/Navbar.svelte
@@ -6,5 +6,5 @@
 	class="flex flex-row items-center p-6 h-20 w-full sticky top-0
 	bg-neutral-50 dark:bg-neutral-950 z-30"
 >
-	<a href="/" class="font-bold"><Logo /></a>
+	<a href="/" class="font-bold text-lg">Mono</a>
 </nav>
