05925ed29bdb5d66a45b24a4940f6501563a2f29
Xyphyn <james@xylight.dev>
2023-09-04 11:52:22 -0700

    misc: add 102756props.class

--- src/lib/popover/Menu.svelte
+++ src/lib/popover/Menu.svelte
@@ -5,9 +5,10 @@
 	export let openOnHover: boolean = false
 	export let origin: Origin = 'bottom-left'
 	export let open: boolean = false
+	export let targetClass: string = ''
 </script>
 
-<Popover {openOnHover} {origin} bind:open class="z-50">
+<Popover {openOnHover} {origin} {targetClass} bind:open class="z-50 {$$props.class}">
 	<slot name="target" slot="target" />
 	<div slot="popover" class="min-w-[16rem] origin-top-left max-h-[32rem] overflow-auto list-none">
 		<Material color="distinct" padding="none" rounding="md" class="py-2 list-none" elevation="max">
