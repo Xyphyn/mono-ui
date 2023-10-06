fd738e5b32e4001ee3022e8a44a92aebd3d4ebf4
Xyphyn <james@xylight.dev>
2023-10-06 15:14:11 -0700

    fix: pass placement props to menu

--- src/lib/popover/Menu.svelte
+++ src/lib/popover/Menu.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
 	import Material from '$lib/materials/Material.svelte'
 	import Popover, { type Origin } from '$lib/popover/Popover.svelte'
+	import {
+		offset,
+		type Middleware,
+		type Placement,
+		shift,
+		type Strategy,
+		flip
+	} from '@floating-ui/core'
 
 	export let openOnHover: boolean = false
 	export let open: boolean = false
+
+	export let placement: Placement = 'bottom-start'
+	export let middleware: Middleware[] = [offset(4), flip(), shift()]
+	export let strategy: Strategy = 'absolute'
 </script>
 
-<Popover {openOnHover} {origin} bind:open class={$$props.class}>
+<Popover
+	{openOnHover}
+	{placement}
+	{middleware}
+	{strategy}
+	{...$$restProps}
+	bind:open
+	class={$$props.class}
+>
 	<slot name="target" slot="target" />
 	<div
 		slot="popover"
