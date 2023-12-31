0eada1242c32982569ea7fca06fe84a810b9dfca
Xylight <james@xylight.dev>
2023-12-30 16:21:36 -0800

    design: improve contrast of menu divider

--- src/lib/popover/MenuDivider.svelte
+++ src/lib/popover/MenuDivider.svelte
@@ -1,3 +1,3 @@
-<span class="text-slate-600 dark:text-zinc-500 text-xs font-medium py-2 px-4 block">
+<span class="text-slate-800 dark:text-zinc-200 text-xs font-medium py-2 px-4 block">
 	<slot />
 </span>
